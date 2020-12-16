5dddecbc91493d000b49e953ec71e928c39e98a7
tony2tones <tony2tones@github.com>
2020-12-16 16:35:56 +0200

    Added routing and added simple navbar added pages added routing for said pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,38 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { PageOneComponent } from './components/page-one/page-one.component';
+import { PageTwoComponent } from './components/page-two/page-two.component';
+import { PageThreeComponent } from './components/page-three/page-three.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'page-one', component: PageOneComponent },
+  { path: 'page-two', component: PageTwoComponent },
+  { path: 'page-three', component: PageThreeComponent },
+  { path: '**', component: HomeComponent }
+
+]; // sets up routes constant where you define your routes
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PageOneComponent,
+    PageTwoComponent,
+    PageThreeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent]
 })
